5e7a4bc7c40e36b8efd9bc93d6f8702aee56f7e6
James Campbell <james@supmenow.com>
2016-11-28 09:54:00 +0000

    Preparing new Botmaster integration.

---
+++ examples/ScrabbleDictionary/index.js
@@ -0,0 +1,31 @@
+var Bottr = require('bottr')
+var fs = require('fs');
+var bot = new Bottr.Bot();
+
+var contents = fs.readFileSync(__dirname + '/words.csv', 'utf8');
+var dictionary = contents.split('\n').filter(function(word){
+  return word.length > 1 && /^[a-zA-Z]+$/.test(word)
+})
+
+bot.on('message_received', function(message, session) {
+
+  var letters = message.text.split("").filter(function(letter){
+      return letter != " "
+  })
+
+  var words = dictionary.filter(function(word) {
+
+    var detectedLetters = letters.filter(function(letter){
+      return word.indexOf(letter) >= 0
+    })
+
+    return detectedLetters.length >= word.length
+  })
+
+  words = words.slice(0, Math.min(30, words.length - 1))
+
+  session.send("You can make these words with the letters: " + letters.join(" "))
+  session.send(words.join(", "))
+});
+
+module.exports = bot
---
+++ examples/TrainTimetable/index.js
@@ -0,0 +1,44 @@
+var Bottr = require('bottr')
+var bot = new Bottr.Bot('TrainTimesBot');
+
+var timetable = [
+  {
+    time: "7:00",
+    destination: "London"
+  },
+  {
+    time: "8:00",
+    destination: "Swindon"
+  },
+  {
+    time: "9:00",
+    destination: "London"
+  },
+  {
+    time: "10:00",
+    destination: "Swindon"
+  },
+  {
+    time: "11:00",
+    destination: "London"
+  },
+  {
+    time: "12:00",
+    destination: "Swindon"
+  }
+]
+
+bot.hears(/timetable/i, function(message, session) {
+
+  var rows = timetable.map(function(item){
+    return item.time + " " + item.destination
+  })
+
+  session.send("The timetable for today is: \n" + rows.join("\n"))
+});
+
+bot.hears([/thanks/i, /cheers/i, /thank you/i, /ta/i, /thx/i, /ty/i], function(message, session) {
+  session.send("No problem!")
+});
+
+module.exports = bot
